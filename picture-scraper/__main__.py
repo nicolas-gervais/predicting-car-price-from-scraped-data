e5d69d6d3c739c30b7da1bd6f96adbb581c0d74a
Nicolas Gervais <46652050+nicolas-gervais@users.noreply.github.com>
2019-12-31 15:38:24 -0500

    list comprehension f string

--- picture-scraper/__main__.py
+++ picture-scraper/__main__.py
@@ -4,11 +4,11 @@ path = 'c:/users/nicolas/documents/data/thecarconnection/picturescraper/'
 os.chdir(path)
 
 directory = 'c:/users/nicolas/desktop/scrap/'
-py = 'python '
 
+files = ['scrape', 'tag', 'save', 'select']
 
 if __name__ == '__main__':
-    os.system(py + path + 'scrape.py ' + directory)
-    os.system(py + path + 'tag.py ' + directory)
-    os.system(py + path + 'save.py ' + directory)
-    os.system(py + path + 'select.py ' + directory)
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+    [os.system('python ' + path + f'{file}.py ' + directory) for file in files]
